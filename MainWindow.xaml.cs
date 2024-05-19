c60456d1a88c3657d57c397a4c4533d8aace8d1a
aney9 <isip_t.a.murashov@mpt.ru>
2024-05-19 23:16:10 +0300

    pizdec

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Windows.Themes;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace messenger2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            TcpServer server = new TcpServer();
+        }
+
+        private void Vhod(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(vhod.Text))//проверка ПОЗАИМСТВОВАНА из гпт, зато робит
+            {
+                MessageBox.Show("Введи логин, че читать не умеешь");
+                vhod.Text = null;
+            }
+            else if (vhod.Text == "Admin" || vhod.Text == "admin" ||  vhod.Text == "Админ" || vhod.Text == "админ")
+            {
+                ServerWindow serverWindow = new ServerWindow();
+                serverWindow.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Ты не админ, бро");
+                vhod.Text = null;
+            }
+        }
+    }
+}
---
+++ TcpServer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace messenger2
+{
+    internal class TcpServer
+    {
+        private Socket socket;
+        private List<Socket> clients = new List<Socket>();
+        ServerWindow window = new ServerWindow();
+        public TcpServer()
+        {
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Any, 8888);
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(ipPoint);
+            socket.Listen(1000);
+            ListenToClients();
+        }
+        private async Task ListenToClients()
+        {
+            while (true)
+            {
+                var client = await socket.AcceptAsync();
+                clients.Add(client);
+                RecieveMessage(client);
+
+            }
+        }
+        private async Task RecieveMessage(Socket client)
+        {
+            while (true)
+            {
+                byte[] bytes = new byte[1024];
+                await client.ReceiveAsync(bytes, SocketFlags.None);
+                string message = Encoding.UTF8.GetString(bytes);
+                window.mess.Items.Add($"[Сообщение от {client.RemoteEndPoint}]: {message}");
+                foreach (var item in clients)
+                {
+                    SendMessage(item, message);
+                }
+            }
+        }
+        private async Task SendMessage(Socket client, string message)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(message);
+
+            await client.SendAsync(bytes, SocketFlags.None);
+        }
+    }
+}
